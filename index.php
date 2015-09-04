1ef226cb31e5df3cee4173a0c87c35e0f67ba2f3
huangsen0912 <1417988198@qq.com>
2015-09-04 20:24:32 +0800

    spl_autoload_register函数的调用加载问题

--- index.php
+++ index.php
@@ -1,18 +1,24 @@
 <?php
+/**
+ * 类->new class1（不认识）正要报错->spl_autoload_register说，我这个有个函数你调用一下，调用了my_load的函数，传递参数，找到Class1类所在的文件，执行
+ */
+function my_load($class){
+	require("class/".$class.'.php');
 
-
+}
+spl_autoload_register('my_load');
 //这是延迟加载模型
 $is_girl = $_GET['girl']==0? true :false;
 if($is_girl){
 
 	echo "this is a girl";
-	require("class\class1.php");
+	//require("class\class1.php");
 	$class1 = new Class1();
 
 }else{
 
 	echo "not a girl";
-	require("class\class2.php");
+	//require("class\class2.php");
 	$class2 = new Class2();
 }
 
